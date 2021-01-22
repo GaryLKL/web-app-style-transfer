6609488da10cbf2e93730a84f1141cbc2bc4bc64
Gary Liu <kll482@nyu.edu>
2021-01-22 15:56:06 -0500

    create a repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='I am following an online tutoriaal to practice how to build a web app with fast-api and streamlit for style-transfer model.',
+    author='Kuan-Lin Liu',
+    license='MIT',
+)
